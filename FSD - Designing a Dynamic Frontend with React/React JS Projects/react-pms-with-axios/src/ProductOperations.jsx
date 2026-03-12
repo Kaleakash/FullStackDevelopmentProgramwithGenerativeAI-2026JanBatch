de71c099f58ba1b2233cc6e90cb017860747422b
Akash <akash300383@gmail.com>
2026-03-12 05:56:59 +0530

    done

--- FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-axios/src/ProductOperations.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-pms-with-axios/src/ProductOperations.jsx
@@ -1,23 +1,40 @@
 import axios from "axios";
 import { useState } from "react";
+export const URL="http://localhost:3000/products";
+
 function ProductOperation(){
 let [products,setProducts]=useState([]);
 let [error,setError]=useState("")
-const URL="http://localhost:3000/products";
 
-let loadProductsDetails = ()=> {
+// ES5 style 
+let loadProductsDetailsES5Style = ()=> {
     // using fetch function 
     //fetch(URL).then(response=>response.json()).then(result=>console.log(result)).catch(error=>console.log(error)) 
     // using axios 
     //axios.get(URL).then(result=>console.log(result.data)).catch(error=>console.log(error))
     axios.get(URL).then(result=>setProducts(result.data)).catch(error=>setError(error.message))
 }
+// ES6 style 
+let loadProductsDetailsES6Style = async ()=> {
+    // using fetch function 
+    //fetch(URL).then(response=>response.json()).then(result=>console.log(result)).catch(error=>console.log(error)) 
+    // using axios 
+    //axios.get(URL).then(result=>console.log(result.data)).catch(error=>console.log(error))
+    //axios.get(URL).then(result=>setProducts(result.data)).catch(error=>setError(error.message))
+    try{
+    let result = await axios.get(URL);  // await keyword make statement as synchronous await is replacement of then 
+    setProducts(result.data);
+    }catch(error){
+        setError(error.message);
+    }
+    
+}
     return(
         <div>
             <h2>Product Operation with Rest API using Axios</h2>
             <input type="button" value="Load Products" 
-            onClick={loadProductsDetails}/>
-            <p>Number of product are {products.length}</p>
+            onClick={loadProductsDetailsES6Style}/>
+            <p>Number of product are </p>
             <table border="1">
                 <thead>
                     <tr>

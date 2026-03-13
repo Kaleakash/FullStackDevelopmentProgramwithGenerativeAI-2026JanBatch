c5a81c8fe6cd66a5cb85284e11e6ed020895ba1a
Akash <akash300383@gmail.com>
2026-03-13 06:27:00 +0530

    done

--- FSD - Designing a Dynamic Frontend with React/React JS Projects/react-with-props/src/Child2.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-with-props/src/Child2.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 function Child2(props) {
 const [user2,setUser2]=useState("user2@gmail.com")
     return(
-         <div style={{"backgroundColor":"yellow","height":"100px","color":"black"}}>
+        <div style={{"backgroundColor":"yellow","height":"150px","color":"black"}}>
             <h3>Child2 Component</h3>
             <p>Child2 emailId is in child2 components is {user2}</p>
             <p>Parent emailId is in child2 component is {props.pName} and designation is {props.designation}</p>
+            <p>Child1 emailId is in child2 component is {props.child1Value}</p>
         </div>
     )
 }
--- FSD - Designing a Dynamic Frontend with React/React JS Projects/react-with-props/src/Parent.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/react-with-props/src/Parent.jsx
@@ -4,14 +4,21 @@ import Child2 from "./Child2";
 
 function Parent() {
 const [admin,setAdmin]=useState("admin@gmail.com")
+const [user1Value,setUser1Value]=useState();
+
+// let receiveValueFromChild1 = (data)=> {
+//     console.log(data)
+//     setUser1Value(data);
+// }
+//let receiveValueFromChild1=(data)=>user1Value(data);
     return(
         <div style={{"backgroundColor":"gray","color":"white"}}>
             <h3>Parent Component</h3>
             <p>Parent emailId is in parent components is {admin}</p>
-        <Child1 parentName={admin} age="21"></Child1>
-        <Child2 pName={admin} designation="Manager"></Child2>
+        <Child1 parentName={admin} age="21" passValueToParent={(data)=>user1Value(data)}></Child1>
+        <Child2 pName={admin} designation="Manager" child1Value={user1Value}></Child2>
             <p>Parent emailId is in parent components is {admin}</p>
-
+            <p>In parent component child1 value is {user1Value}</p>
         </div>
     )
 }

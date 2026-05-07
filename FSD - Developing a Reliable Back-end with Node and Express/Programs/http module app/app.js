fceac8eb1e329abd5e867ef280dc16bae1262ba7
Akash <akash300383@gmail.com>
2026-05-07 07:39:44 +0530

    done

---
+++ FSD - Developing a Reliable Back-end with Node and Express/Programs/http module app/app.js
@@ -0,0 +1,36 @@
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+let PORT = 3000;
+
+let server = http.createServer((request, response) => {
+    let urlObj = url.parse(request.url, true);
+    // response.writeHead(200, { 'Content-Type': 'text/html' });
+    if (urlObj.pathname === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        let indexPage = fs.readFileSync('index.html');
+        response.write(indexPage);
+    }else if (urlObj.pathname === '/about_us') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        let aboutPage = fs.readFileSync('aboutus.html');
+        response.write(aboutPage);
+    } else if(urlObj.pathname === '/contact_us') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        let contactPage = fs.readFileSync('contactus.html');
+        response.write(contactPage);
+    }else if(urlObj.pathname === '/login') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });   
+        let loginPage = fs.readFileSync('login.html');
+        response.write(loginPage);
+    }else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.write('<h1>404 Not Found</h1>');
+    }
+
+    response.end();
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

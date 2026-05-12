5317d2767308e20bcf7d4a5be0fb81cacb7ecd6c
Akash <akash300383@gmail.com>
2026-05-12 06:08:24 +0530

    done

--- FSD - Developing a Reliable Back-end with Node and Express/Programs/express-view-engine-ejs/app.js
+++ FSD - Developing a Reliable Back-end with Node and Express/Programs/express-view-engine-ejs/app.js
@@ -10,5 +10,19 @@ app.get("/",(request,response)=>{
     response.render('index',{message: msg});
 });
 
+app.get("/about",(request,response)=>{
+    let aboutMsg = "This is the about page of our Express JS application"
+    response.render('about',{aboutMessage: aboutMsg});
+}
+);
+
+app.get("/contact",(request,response)=>{    
+    let contactMsg = "This is the contact page of our Express JS application"
+    response.render('contact',{contactMessage: contactMsg});
+});
+
+app.get("/login",(request,response)=>{
+    response.render('login');
+})
 
 app.listen(3000,()=>console.log('Server is running on port 3000'));

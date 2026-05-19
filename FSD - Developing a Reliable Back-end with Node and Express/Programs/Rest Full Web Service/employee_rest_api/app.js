773e27f98b7c79c8de380ddc5ad88cc6b597acdc
Akash <akash300383@gmail.com>
2026-05-19 05:36:38 +0530

    done

--- FSD - Developing a Reliable Back-end with Node and Express/Programs/Rest Full Web Service/employee_rest_api/app.js
+++ FSD - Developing a Reliable Back-end with Node and Express/Programs/Rest Full Web Service/employee_rest_api/app.js
@@ -25,6 +25,7 @@ app.get('/findEmployeeById/:id', (req, res) => {
         res.status(404).json({message: 'Employee not found'});
     }
 })
+
 // http://localhost:3000/addEmployee
 // method : post 
 //data : {id: 4, name: 'Bob Brown', position: 'QA Engineer'}
@@ -40,7 +41,34 @@ app.post("/addEmployee", (req, res) => {
     }
 });
 
+// http://localhost:3000/updateEmployeePosition
+// method : patch 
+//data :  {id: 1, position: 'Senior Software Engineer'}
 
+app.patch("/updateEmployeePosition", (req, res) => {
+    let newEmployee = req.body;
+    let employeeIndex = employees.findIndex(emp => emp.id === newEmployee.id);
+    if(employeeIndex<0) {
+        res.status(400).json({message: `Employee not present}`});
+    } else {
+        employees[employeeIndex].position=newEmployee.position;
+        res.status(201).json({message: 'Employee position Updated successfully'});
+    }
+});
+
+// http://localhost:3000/deleteEmployeeById/1
+// method : delete 
+
+app.delete("/deleteEmployeeById/:id", (req, res) => {
+    let id = parseInt(req.params.id);
+    let employeeIndex = employees.findIndex(emp => emp.id === id);
+    if(employeeIndex<0) {
+       res.status(400).json({message: `Employee not present}`});
+    } else {
+        employees.splice(employeeIndex,1);      // 1st parameter index position and 2nd parameter index position
+        res.status(201).json({message: 'Employee deleted successfully'});
+    }
+});
 
 
 
